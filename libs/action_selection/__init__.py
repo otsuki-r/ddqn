20ed45b0a4d26067bd10956da09f75913391384c
ro <lei.otsuki@gmail.com>
2025-01-13 22:08:17 +0000

    Fix import name

--- libs/action_selection/__init__.py
+++ libs/action_selection/__init__.py
@@ -3,4 +3,4 @@ from .action_selection import (
     make_epsilon_greedy_with_bloom_filter,
 )
 
-__all__ = ["make_epsilon_greedy", "make_epsilon_greed_with_bloom"]
+__all__ = ["make_epsilon_greedy", "make_epsilon_greedy_with_bloom_filter"]
