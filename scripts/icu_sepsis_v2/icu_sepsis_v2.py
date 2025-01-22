d67444b6ced026ca5474385475bc46257d5aeca4
ro <lei.otsuki@gmail.com>
2025-01-22 22:59:55 +0000

    Add in sepsis scripts

---
+++ scripts/icu_sepsis_v2/icu_sepsis_v2.py
@@ -0,0 +1,43 @@
+"""Quickstart example for the ICU-Sepsis environment."""
+
+import logging
+import os
+import sys
+
+from ddqn.configure import ddqn_parser, process_cli_args
+from ddqn.structures import DoubleDQN, DoubleReplayMemory, StateChange
+from ddqn.utils import run, train
+
+from utils import (
+    build_env_config,
+    build_run_config,
+    build_training_config,
+    custom_eval,
+)
+
+LOG_LEVEL = os.environ.get("LOG_LEVEL", "INFO").upper()
+logging.basicConfig(level=LOG_LEVEL, stream=sys.stdout)
+logging.getLogger("ddqn").setLevel(logging.DEBUG)
+
+
+if __name__ == "__main__":
+    cli_args = process_cli_args(ddqn_parser)
+    env_config = build_env_config(cli_args)
+
+    replay_memory = DoubleReplayMemory[StateChange](
+        capacity=cli_args.memory_size,
+        warmup_episodes_upper=cli_args.warmup_episodes_upper,
+        main_episodes_lower=cli_args.main_episodes_lower,
+    )
+    ddqn = DoubleDQN(
+        replay_memory=replay_memory,
+        n_observations=env_config.state_space_size,
+        n_actions=env_config.action_space_size,
+    )
+
+    if cli_args.eval:
+        run_config = build_run_config(cli_args)
+        custom_eval(ddqn, env_config=env_config, run_config=run_config)
+    else:
+        training_config = build_training_config(ddqn, cli_args, env_config)
+        train(ddqn, env_config=env_config, training_config=training_config)
---
+++ scripts/icu_sepsis_v2/utils.py
@@ -0,0 +1,77 @@
+import argparse
+import logging
+import pathlib
+
+import icu_sepsis  # noqa: F401
+import gymnasium as gym
+import torch
+import torch.optim as optim
+import torch.nn as nn
+from ddqn.action_selection import make_epsilon_greedy
+from ddqn.adaptors import make_torch_from_int
+from ddqn.structures import DoubleDQN
+from ddqn.configure import EnvConfig, RunConfig, TrainingConfig
+from ddqn.utils import run
+
+
+def build_env_config(cli_args: argparse.Namespace) -> EnvConfig:
+    env = gym.make(
+        # "Sepsis/ICU-Sepsis-v2", inadmissible_action_strategy="terminate"
+        "Sepsis/ICU-Sepsis-v2"
+    )
+    return EnvConfig(
+        env=env,
+        action_space_size=25,
+        state_space_size=716,
+    )
+
+
+def build_training_config(
+    ddqn: DoubleDQN, cli_args: argparse.Namespace, env_config: EnvConfig
+) -> TrainingConfig:
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    return TrainingConfig(
+        epsilon_start=cli_args.epsilon_start,
+        epsilon_end=cli_args.epsilon_end,
+        epsilon_decay=cli_args.epsilon_decay,
+        num_episodes=cli_args.num_episodes,
+        batch_size=cli_args.batch_size,
+        gamma=cli_args.gamma,
+        optimiser=optim.AdamW(
+            ddqn.pnet.parameters(), lr=cli_args.alpha, amsgrad=True
+        ),
+        tau=cli_args.tau,
+        action_selector_fn=make_epsilon_greedy(
+            device=device, env=env_config.env
+        ),
+        loss_fn=nn.SmoothL1Loss(),  # Generalisation of robust Huber loss
+        device=device,
+        early_return_fn=None,
+        outdir=pathlib.Path(cli_args.outdir),
+        state_space_adaptor=make_torch_from_int(env_config.state_space_size),
+        state_space_dtype=torch.float32,
+    )
+
+
+def build_run_config(cli_args: argparse.Namespace) -> RunConfig:
+    outdir = pathlib.Path(cli_args.outdir)
+    spath = pathlib.Path(cli_args.save_path) if cli_args.save_path else None
+    return RunConfig(weights_dir=outdir, save_path=spath, num_iterations=1)
+
+
+def custom_eval(
+    ddqn: DoubleDQN, env_config: EnvConfig, run_config: RunConfig
+) -> None:
+    rewards: list[int] = []
+    durations: list[int] = []
+    for _ in range(1_000):
+        res = run(ddqn, env_config=env_config, run_config=run_config)
+        logging.info(res)
+        rewards.append(res.total_reward)
+        durations.append(res.epsiode_duration)
+    logging.info("Evaluation (mean of 1,000 runs):")
+    logging.info(f"Rewards: {sum(rewards) / len(rewards):.3f}")
+    logging.info(f"Epsidoe duration: {sum(durations) / len(durations):.3f}")
+    return
+    return (sum(rewards) / len(rewards),)
