c617c5761dc79239ba9c3b85daffecf424723790
ro <lei.otsuki@gmail.com>
2025-01-22 23:00:35 +0000

    Add make_torch_from_int util

--- ddqn/adaptors/wrangle.py
+++ ddqn/adaptors/wrangle.py
@@ -1,4 +1,4 @@
-from typing import TypeVar
+from typing import Callable, TypeVar
 
 import numpy as np
 import torch
@@ -10,3 +10,14 @@ def torch_from_array(
     vals: list[T] | np.ndarray, *, device: torch.device, dtype=torch.dtype
 ) -> torch.Tensor:
     return torch.tensor(vals, device=device, dtype=dtype)
+
+
+def make_torch_from_int(dim_state_space: int) -> Callable[[...], torch.Tensor]:
+    def torch_from_int(
+        val: int, *, device: torch.device, dtype: torch.dtype
+    ) -> torch.Tensor:
+        z = torch.zeros(dim_state_space, device=device, dtype=dtype)
+        z[val] = 1
+        return z
+
+    return torch_from_int
