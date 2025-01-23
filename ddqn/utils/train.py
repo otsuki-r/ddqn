a5e35b823c1c0d206017f05725387271bd54fad5
ro <lei.otsuki@gmail.com>
2025-01-23 01:06:41 +0000

    Fix call to `EarlyReturn` in `train`

--- ddqn/utils/train.py
+++ ddqn/utils/train.py
@@ -149,10 +149,14 @@ def train(
                 state = next_state
         pbar.update()
 
-        if training_config.early_return_fn and training_config.early_return_fn(
-            episode_durations=episode_durations, rewards=rewards, losses=losses
-        ):
-            break
+        # Check if early return has been satisfied
+        if episode_num > ddqn.replay_memory.main_episodes_lower:
+            training_config.early_return_fn.update(this_episode_reward)
+            if (
+                episode_num % training_config.early_return_steps == 0
+                and training_config.early_return_fn.evaluate()
+            ):
+                break
 
     plot_episode_durations(episode_durations, outdir / "episode_durations.png")
     plot_losses(losses, outdir / "losses.png")
