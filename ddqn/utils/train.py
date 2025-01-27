dbbee7c9bd2eb0ec2c0b625ebd0cf1c6cf83836c
ro <lei.otsuki@gmail.com>
2025-01-27 20:51:05 +0000

    Increase logging frquency

--- ddqn/utils/train.py
+++ ddqn/utils/train.py
@@ -65,7 +65,7 @@ def train(
     logger.debug("Starting training...")
     start = time.time()
     global_step_number = 1
-    k = 1_000
+    k = 100
     for episode_num in range(1, training_config.num_episodes + 1):
         this_episode_reward: float = 0.0
 
